486a50c60d903dbc86a21c1dd802d9ab1804df2c
Hyejin <hyejin.jeon940120@gmail.com>
2026-01-08 15:15:43 +0900

    init commit

---
+++ src/test/java/com/tracker/subscriptionvaluetracker/SubscriptionValueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.subscriptionvaluetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionValueTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
